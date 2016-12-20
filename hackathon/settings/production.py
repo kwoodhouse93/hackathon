4b19c3227066fd4ca7bd99bc3f15b673c64e252c
Robert Williams <robert.williams@hds.com>
2016-12-20 11:58:23 +0000

    Split settings into production and dev, so we can use different databases for each

---
+++ hackathon/settings/production.py
@@ -0,0 +1,13 @@
+from .defaults import *
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT' : 5432,
+    }
+}
